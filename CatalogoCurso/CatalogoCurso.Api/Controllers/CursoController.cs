a152f2181814ad289b56ae65b89bf595cf7f1f46
rafaelaport <rafaela.portugal96@gmail.com>
2022-08-17 23:35:35 -0300

    Apontando para banco na nuvem

---
+++ CatalogoCurso/CatalogoCurso.Api/Controllers/CursoController.cs
@@ -0,0 +1,25 @@
+﻿using CatalogoCurso.Domain.Curso.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogoCurso.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CursoController : ControllerBase
+    {
+
+        public ICursoRepository CursoRepository { get; }
+
+        public CursoController(ICursoRepository cursoRepository) 
+        {
+            CursoRepository = cursoRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ObterTodos()
+        {
+            return Ok(await this.CursoRepository.ObterTodos());
+        }
+    }
+}
